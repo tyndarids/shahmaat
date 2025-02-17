f9d8efcb47538b6e87bc07bdb230194010fe3e84
Ilesh Thiada <ileshkt@gmail.com>
2025-02-17 15:04:16 +0530

    Establish websocket communication with subprotocol version checking

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,64 @@
-fn main() {
-    println!("Hello, world!");
+use futures_util::{SinkExt as _, stream::StreamExt as _};
+use http::header::SEC_WEBSOCKET_PROTOCOL;
+use tokio::net::TcpListener;
+use tokio_tungstenite::tungstenite::{
+    handshake::client::Request,
+    http::{HeaderValue, Response},
+};
+
+const PROTOCOL_VERSION: &str = "shahmaat_protocol_0.1.0";
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let socket = TcpListener::bind("localhost:8080").await?;
+
+    eprint!("Waiting for connection... ");
+    while let Ok((raw_stream, addr)) = socket.accept().await {
+        println!("Accepted connection from {addr}");
+
+        let stream = tokio_tungstenite::accept_hdr_async(
+            raw_stream,
+            |req: &'_ Request, mut resp: Response<_>| {
+                let protocols = req.headers().get_all(http::header::SEC_WEBSOCKET_PROTOCOL);
+                for protocol in protocols
+                    .iter()
+                    .map(HeaderValue::to_str)
+                    .map(Result::unwrap)
+                    .map(str::trim)
+                {
+                    if protocol == PROTOCOL_VERSION {
+                        resp.headers_mut()
+                            .append(SEC_WEBSOCKET_PROTOCOL, protocol.try_into().unwrap());
+                        return Ok(resp);
+                    }
+                }
+                eprintln!("Shahmaat protocol version mismatch");
+                eprintln!("Client protocols: {protocols:?}");
+                eprintln!("Server protocols: {PROTOCOL_VERSION:?}");
+                Ok(resp)
+            },
+        )
+        .await?;
+        let (mut tx, mut rx) = stream.split();
+
+        while let Some(message) = rx.next().await {
+            let message = message?;
+            use tokio_tungstenite::tungstenite::Message;
+            match message {
+                Message::Text(text) => {
+                    println!("Received: {text}");
+                    tx.send(Message::Text("Hello from Rust!".into())).await?;
+                    println!("Sent message");
+                }
+                Message::Binary(bytes) => todo!(),
+                Message::Ping(bytes) => todo!(),
+                Message::Pong(bytes) => todo!(),
+
+                Message::Close(_) => break,
+                Message::Frame(_) => unreachable!(),
+            }
+        }
+    }
+
+    Ok(())
 }
