2a867ba2e77bcac92e82adc95f89af2f37c8b4d8
Ilesh Thiada <ileshkt@gmail.com>
2025-03-05 22:51:23 +0530

    Debug watermark and fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:web_socket_channel/io.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
@@ -6,18 +9,44 @@ import 'playfield.dart';
 
 const protocolVersion = "0.1.0";
 
-void main() => runApp(const Shahmaat());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(Shahmaat(packageInfo: await PackageInfo.fromPlatform()));
+}
 
 class Shahmaat extends StatelessWidget {
-  const Shahmaat({super.key});
+  const Shahmaat({super.key, required this.packageInfo});
+
+  final PackageInfo packageInfo;
 
   @override
   Widget build(BuildContext context) => MaterialApp(
     title: "Shahmaat",
     theme: ThemeData(
       colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      textTheme: GoogleFonts.interTextTheme(),
     ),
-    home: const MyHomePage(),
+    home: Stack(
+      children: [
+        const MyHomePage(),
+        kDebugMode
+            ? Positioned(
+              bottom: 8,
+              right: 8,
+              child: Text(
+                "${packageInfo.appName} client\nversion ${packageInfo.version}\nshahmaat_protocol_$protocolVersion",
+                style: GoogleFonts.jetBrainsMono().copyWith(
+                  fontSize: 14,
+                  color: Colors.black54,
+                  decoration: TextDecoration.none,
+                ),
+                textAlign: TextAlign.right,
+              ),
+            )
+            : SizedBox.shrink(),
+      ],
+    ),
   );
 }
 
@@ -38,12 +67,6 @@ class _MyHomePageState extends State<MyHomePage> {
   WebSocketChannel? _channel = _establishConnection();
 
   @override
-  void dispose() async {
-    _channel?.sink.close();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) => Scaffold(
     appBar: AppBar(
       backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -60,7 +83,13 @@ class _MyHomePageState extends State<MyHomePage> {
                 ? Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Text("Error: ${snapshot.error}"),
+                    Padding(
+                      padding: const EdgeInsets.all(8),
+                      child: Text(
+                        "${snapshot.error}",
+                        style: GoogleFonts.jetBrainsMono(),
+                      ),
+                    ),
                     Padding(
                       padding: const EdgeInsets.all(8),
                       child: FilledButton.icon(
